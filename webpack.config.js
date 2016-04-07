7aae939f21a55c943d1df9443a77c44896a69a7f
Sebastian Qu <sebastianqu@gmail.com>
2016-04-07 17:11:37 +0800

    add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,35 @@
+var webpack = require('webpack');
+var path = require('path');
+var htmlWebpackPlugin = require('html-webpack-plugin');
 
 var config = {
+    devtool: 'inline-source-map',
+    context: __dirname + 'app'
     entry: [
-        './app/index.js',
-    ]
+        './index.js',
+    ],
+    plugins: [
+        new htmlWebpackPlugin({
+            inject: 'head',
+            template: 'index.html'
+        })
+    ],
+    output: {
+        path: __dirname + 'app',
+        filename: './build/bundle.js'
+    },
+    resolve: {
+        root: __dirname + 'app',
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            include: [
+                path.resolve(__dirname + 'app')
+            ],
+            loader: 'babel-loader'
+        }]
+    }
 };
 
 module.exports = config;
