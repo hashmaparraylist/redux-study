5d819a924d5f52b139e921768b7454ade221220c
Sebastian Qu <sebastianqu@gmail.com>
2016-04-14 14:05:12 +0800

    change package name

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,11 @@
-import React from 'React';
+import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './containers/App';
 import todoApp from './reducers';
 
+// 创建store
 let store = createStore(todoApp);
 
 let rootElement = document.getElementById('root');
